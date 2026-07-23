4951e3224d607528922b10fec04b00b478932b2e
masudrananayeem <sadnayeem6393@gmail.com>
2026-07-24 01:55:25 +0600

    update cors dependency to version 2.8.6 and add additional localhost origins for development

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,8 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
       "https://my-portfolio-gold-seven-79.vercel.app",
     ],
     credentials: true,
