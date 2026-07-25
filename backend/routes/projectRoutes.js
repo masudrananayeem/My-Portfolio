585f72d5aee33fd762ef14625e04d54c41d06c46
masudrananayeem <sadnayeem6393@gmail.com>
2026-07-25 10:35:06 +0600

    updating the file for url to upload any picture from my pc#

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { upload, handleMulterError } = require('../middleware/uploadMiddleware');
 const {
   getProjects,
   getProjectBySlug,
@@ -9,10 +10,13 @@ const {
 } = require('../controllers/projectController');
 const { protect } = require('../middleware/authMiddleware');
 
+// Public routes
 router.get('/', getProjects);
 router.get('/:slug', getProjectBySlug);
-router.post('/', protect, createProject);
-router.put('/:id', protect, updateProject);
+
+// Admin routes with file upload - IMPORTANT: field name must be 'image'
+router.post('/', protect, upload.single('image'), handleMulterError, createProject);
+router.put('/:id', protect, upload.single('image'), handleMulterError, updateProject);
 router.delete('/:id', protect, deleteProject);
 
 module.exports = router;
